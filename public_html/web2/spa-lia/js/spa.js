3fe69a573ae2ed0061687bc0e43b45061eb0c288
Kelly <2555156@edu.vaniercollege.qc.ca>
2026-04-29 02:40:21 -0400

    updated version

--- public_html/web2/spa-lia/js/spa.js
+++ public_html/web2/spa-lia/js/spa.js
@@ -1,24 +1,26 @@
 // console.log('spa.js is loaded');
 
 // modal
-const thumbnails = document.querySelectorAll('figure');
+// const thumbnails = document.querySelectorAll('figure');
 
-const modal = document.querySelector('dialog');
+// const modal = document.querySelector('dialog');
 
-thumbnails.forEach(thumb => {
-    thumb.addEventListener('click', showModal);
+// thumbnails.forEach(thumb => {
+//     thumb.addEventListener('click', showModal);
 
-})
+// })
 
-function showModal(event){
-let thumbImg = event.target.closest('.information figure').querySelector('img').src;
-    thumbImg = thumbImg.substring(thumbImg.lastIndexOf('/'));
+// function showModal(event){
+// let thumbImg = event.target.closest('.information figure').querySelector('img').src;
+//     thumbImg = thumbImg.substring(thumbImg.lastIndexOf('/'));
 
-    modal.querySelector('figure').innerHTML = event.target.closest('figure').innerHTML;
-    modal.querySelector('img').src = 'img/flowers/1600' + thumbImg;
-    modal.showModal(); 
-}
-// loader
+//     modal.querySelector('figure').innerHTML = event.target.closest('figure').innerHTML;
+//     modal.querySelector('img').src = 'img/flowers/1600' + thumbImg;
+//     modal.showModal(); 
+// }
+
+
+// // loader
 window.addEventListener("load", () => {
   const loader = document.querySelector(".container");
   loader.classList.add("fade-out");
@@ -28,27 +30,38 @@ window.addEventListener("load", () => {
   }, 1000);
 });
 
-// // reveal on scroll
-// document.addEventListener("DOMContentLoaded", () => {
-//   const revealElements = document.querySelectorAll('.reveal');
+// IntersectionObserver
 
-//   const toggleVisibility = () => {
-//       revealElements.forEach((el) => {
-//           const rect = el.getBoundingClientRect();
+const thumbs = document.querySelectorAll('header, #about, #flowers, #common, footer');
+// selects all
 
-//           // check if the element is in the viewport
-//           if (rect.top < window.innerHeight && rect.bottom > 0) {
-//               el.classList.add('active'); 
-//           } else {
-//               el.classList.remove('active'); 
-//           }
-//       });
-//   };
+const options = {
+  root: null,
+  rootMargin: "0px",
+  scrollMargin: "0px",
+  threshold: 0.175, 
+// percentage in which it loads
+};
 
-//   // trigger on scroll and load
-//   window.addEventListener('scroll', toggleVisibility);
-//   toggleVisibility();
-// });
+const observer = new IntersectionObserver(removeOpacity, options);
+
+thumbs.forEach(thumb => {
+  observer.observe(thumb);
+});
+
+function removeOpacity(entries){
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+      // fades in
+     
+    } else {
+      entry.target.classList.remove('visible');
+    }
+    // fades out
+
+  });
+};
 
 // carousel
 const carousel = document.querySelector('.information');
