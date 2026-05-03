254d5c6df850b2150d9e709783e7985eb705d44f
Kelly <2555156@edu.vaniercollege.qc.ca>
2026-05-03 01:22:55 -0400

    sooo complicated..

--- public_html/web2/spa-lia/js/spa.js
+++ public_html/web2/spa-lia/js/spa.js
@@ -1,39 +1,43 @@
 // console.log('spa.js is loaded');
 
 // modal
-  const thumbnails = document.querySelectorAll('.information figure');
-  const modal = document.querySelector('dialog');
+const figures = document.querySelectorAll('.information figure');
+const modal = document.querySelector('dialog');
 
-  figure.forEach(thumb => {
-  figure.addEventListener('click', showModal);
+figures.forEach(figure => {
+figure.addEventListener('click', showModal);
  })
 
-  function showModal(event){
+function showModal(event){
   const figure = event.currentTarget;
 
   const img = figure.querySelector('img').src;
   const title = figure.querySelector('figcaption').textContent;
   const desc = figure.dataset.desc;
 
-  modal.querySelector('img').src = img;
+  console.log('original img src:', img);
+  console.log('replaced img src:', img.replace('flowers/400/', 'flowers/1600/'));
+
+  modal.querySelector('img').src = img.replace('flowers/400/', 'flowers/1600/');
   modal.querySelector('h2').textContent = title;
   modal.querySelector('p').textContent = desc;
    
-  modal.showModal(); 
+modal.showModal(); 
 }
 modal.addEventListener('click', function(e) {
-  if (e.target ===modal) modal.close
+  if (e.target ===modal) modal.close();
 })
 
 
 // // loader
 window.addEventListener("load", () => {
   const loader = document.querySelector(".container");
+  console.log(loader);
   loader.classList.add("fade-out");
 
   setTimeout(() => {
     loader.style.display = "none";
-  }, 1000);
+  }, 500);
 });
 
 // IntersectionObserver
@@ -44,8 +48,8 @@ const thumbs = document.querySelectorAll('header, #about, #flowers, #common, foo
 const options = {
   root: null,
   rootMargin: "0px",
-  scrollMargin: "0px",
-  threshold: 0.175, 
+  // scrollMargin: "0px",
+  threshold: 0.1, 
 // percentage in which it loads
 };
 
